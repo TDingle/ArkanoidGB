5f1bfb56eb69ee0471b49edb4d29e066a912ef85
TDingle <102336688+TDingle@users.noreply.github.com>
2025-04-16 14:06:24 +0700

    Player Input
    Player can control the movement of the paddle, instead of it moving automatically

--- main.asm
+++ main.asm
@@ -78,21 +78,44 @@ WaitVBlank2:
 	cp 144
 	jp c, WaitVBlank2
 
-	ld a, [wFrameCounter]
-	inc a
-	ld [wFrameCounter], a
-	cp a, 15 ; Every 15 frames (1/4 of a second), run the following code
-	jp nz, Main
+	; Check the current keys every fram and move left or right
+	call UpdateKeys
 
-	; Reset the frame counter to 0
-	ld a, 0
-	ld [wFrameCounter], a
+	; First, check if the left button is pressed
+CheckLeft:
+	ld a, [wCurKeys]
+	and a, PADF_LEFT
+	jp z, CheckRight
+Left:
+	; move the paddle one pixel to the left
+	ld a, [_OAMRAM + 1]
+	dec a
+	; If we've already hit the edge of the playfield , don't move
+	cp a, 15
+	jp z, Main
+	ld [_OAMRAM + 1], a
+	jp Main
 
+	;Check the right button
+CheckRight:
+	ld a, [wCurKeys]
+	and a, PADF_RIGHT
+	jp z, Main
+Right:
 	; Move the paddle one pixel to the right
 	ld a, [_OAMRAM + 1]
 	inc a
+	; if we already hit the edge of the playfield, don't move
+	cp a, 105
+	jp z, Main
 	ld [_OAMRAM + 1], a
 	jp Main
+
+	; Initialize global variables
+	ld a, 0
+	ld [wFrameCounter], a
+	ld [wCurKeys], a
+	ld [wNewKeys], a
 	
 	; Copy bytes from one area to another
 	; @param de: Source
@@ -107,6 +130,43 @@ Memcopy:
 	or a, c
 	jp nz, Memcopy
 	ret
+
+	UpdateKeys:
+	; Poll half the controller
+	ld a, P1F_GET_BTN
+	call .onenibble
+	ld b, a ; B7-4 = 1; B3-0 = unpressed buttons
+  
+	; Poll the other half
+	ld a, P1F_GET_DPAD
+	call .onenibble
+	swap a ; A7-4 = unpressed directions; A3-0 = 1
+	xor a, b ; A = pressed buttons + directions
+	ld b, a ; B = pressed buttons + directions
+  
+	; And release the controller
+	ld a, P1F_GET_NONE
+	ldh [rP1], a
+  
+	; Combine with previous wCurKeys to make wNewKeys
+	ld a, [wCurKeys]
+	xor a, b ; A = keys that changed state
+	and a, b ; A = keys that changed to pressed
+	ld [wNewKeys], a
+	ld a, b
+	ld [wCurKeys], a
+	ret
+  
+  .onenibble
+	ldh [rP1], a ; switch the key matrix
+	call .knownret ; burn 10 cycles calling a known ret
+	ldh a, [rP1] ; ignore value while waiting for the key matrix to settle
+	ldh a, [rP1]
+	ldh a, [rP1] ; this read counts
+	or a, $F0 ; A7-4 = 1; A3-0 = unpressed keys
+  .knownret
+	ret
+
 Tiles:
 	dw `33333333
 	dw `33333333
@@ -321,3 +381,7 @@ PaddleEnd:
 
 SECTION "Counter", wram0
 wFrameCounter: db
+
+SECTION "Input Variables", WRAM0
+wCurKeys: db
+wNewKeys: db
